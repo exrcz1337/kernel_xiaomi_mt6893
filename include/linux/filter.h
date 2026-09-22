202083bbc7e5e0f53818489c0afcca3b4b0b1c70
exerczz1 <exrcz513@gmail.com>
2026-09-22 21:35:33 +0600

    Revert "HACK: bpf: shim BPF_FUNC_get_netns_cookie"
    This reverts commit 83fe1d3a6bde9bdf7747135da50e1c8351238b16.

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -1472,6 +1472,4 @@ static inline bool bpf_sk_lookup_run_v4(struct net *net, int protocol,
 	return no_reuseport;
 }
 
-u64 __bpf_get_netns_cookie(struct sock *sk);
-
 #endif /* __LINUX_FILTER_H__ */
--- net/core/filter.c
+++ net/core/filter.c
@@ -4729,7 +4729,7 @@ static const struct bpf_func_proto bpf_get_socket_cookie_sock_ops_proto = {
 	.arg1_type	= ARG_PTR_TO_CTX,
 };
 
-u64 __bpf_get_netns_cookie(struct sock *sk)
+static u64 __bpf_get_netns_cookie(struct sock *sk)
 {
 	const struct net *net = sk ? sock_net(sk) : &init_net;
 
